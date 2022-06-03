2fa9d03f9ae87047ee3422c201299cfa435e2e63
Slava <slmzig@gmail.com>
2022-06-03 14:01:26 +0300

    add types

--- src/main/scala/docs/basics/Types.md
+++ src/main/scala/docs/basics/Types.md
@@ -1,4 +1,17 @@
+[examples](../../lectures/basics/Types.scala)
+
+in scala we can define variable:
+- with type 
+- without type
+
+```scala
+
+val intValueWithType: Int = 4
+val intValueWithoutType = 4
+```
+
 list of types:
+
 - string
 - int
 - boolean
---
+++ src/main/scala/lectures/basics/Types.scala
@@ -0,0 +1,16 @@
+package lectures.basics
+
+object Types extends App {
+
+  val x: Int = 1
+  val x2 = 1
+
+  val stringVal: String = "string"
+  val intVal: Int = 1
+  val booleanVal: Boolean = true
+  val charVal: Char = 'a'
+  val shortVal: Short = 9999
+  val longVal: Long = 9999
+  val doubleVal: Double = 99.99
+  val floatVal: Float = 99.99f
+}

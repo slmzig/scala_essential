4f70feef922cd972d80ef65c541c25d486a1956f
Slava <slmzig@gmail.com>
2022-06-03 13:28:31 +0300

    refactor Variables.scala

--- src/main/scala/lectures/basics/Variables.scala
+++ src/main/scala/lectures/basics/Variables.scala
@@ -1,45 +1,21 @@
 package lectures.basics
 
 object Variables extends App {
-  println("start ValuesVariablesTypes")
-
 
   // immutable variable
-  val x = 42
-  println(x)
+  val immutableValue = 42
+  println(immutableValue) // 42
   /**
    *
-   *  x = 43
-   *  this will lead to compile error because it is mutable variable
+   * immutableValue = 43
+   * this will lead to compile error because it is mutable variable
    *
    * */
 
-  val x2: Int = 42
-  /**
-   *  as you can see above
-   *  we the type of variable is optional
-   *  compiler defines it by itself
-   * */
+  // mutable value
+  var mutableValue = 3
+  mutableValue = 4
 
-  val x3 = 42;
-  /**
-   * semicolon is optional in scala
-   * semicolon is required only when use write multiple expressions in one line
-   */
+  println(mutableValue) // 4
+
 }
-/**
-variables are:
-- immutable
-- mutable
-
-immutable - means that can not reassign value to it
-mutable - means that we can reassign new value to it
-
-types:
-- declaring the type of variable is optional
-- compiler define type of variable by itself
-
-semicolons:
-- semicolon is optional in scala
-- semicolon is required only when use write multiple expressions in one line
-**/
